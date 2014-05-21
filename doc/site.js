17e9a8b8c25920b241941cb331f8005a79b9fcc0
Ryan Roemer <ryan.roemer@formidablelabs.com>
2014-05-21 16:31:14 -0700

    Oops. Removing titles also broke the site.js add class hack.

--- doc/site.js
+++ doc/site.js
@@ -56,13 +56,13 @@ $(function () {
 
       // Add special picture classes to known titles.
       $("img").each(function () {
-        if (_.indexOf(pictureClass, $(this).prop("title")) !== -1) {
+        if (_.indexOf(pictureClass, $(this).prop("alt")) !== -1) {
           $(this).addClass("picture");
         }
-        if (_.indexOf(bookClass, $(this).prop("title")) !== -1) {
+        if (_.indexOf(bookClass, $(this).prop("alt")) !== -1) {
           $(this).addClass("book");
         }
-        if (_.indexOf(logoClass, $(this).prop("title")) !== -1) {
+        if (_.indexOf(logoClass, $(this).prop("alt")) !== -1) {
           $(this).addClass("logo");
         }
       });
