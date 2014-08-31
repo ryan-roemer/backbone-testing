bd828784198e13b847199d6f347720af537bbc8d
Ryan Roemer <ryan@loose-bits.com>
2014-08-30 18:02:44 -0700

    Open up all the SL combos.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,27 +10,27 @@ var SAUCE_ENVS = {
   //   base: "SauceLabs",
   //   browserName: "firefox"
   // },
-  // sl_chrome: {
-  //   base: "SauceLabs",
-  //   browserName: "chrome"
-  // },
-  // sl_safari: {
-  //   base: "SauceLabs",
-  //   browserName: "safari",
-  //   platform: "OS X 10.9"
-  // },
-  // sl_ie_9: {
-  //   base: "SauceLabs",
-  //   browserName: "internet explorer",
-  //   platform: "Windows 7",
-  //   version: "9"
-  // },
-  // sl_ie_10: {
-  //   base: "SauceLabs",
-  //   browserName: "internet explorer",
-  //   platform: "Windows 7",
-  //   version: "10"
-  // },
+  sl_chrome: {
+    base: "SauceLabs",
+    browserName: "chrome"
+  },
+  sl_safari: {
+    base: "SauceLabs",
+    browserName: "safari",
+    platform: "OS X 10.9"
+  },
+  sl_ie_9: {
+    base: "SauceLabs",
+    browserName: "internet explorer",
+    platform: "Windows 7",
+    version: "9"
+  },
+  sl_ie_10: {
+    base: "SauceLabs",
+    browserName: "internet explorer",
+    platform: "Windows 7",
+    version: "10"
+  },
   sl_ie_11: {
     base: "SauceLabs",
     browserName: "internet explorer",
